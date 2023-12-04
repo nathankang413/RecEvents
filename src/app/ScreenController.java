6f64b4147a4ec17ca1b7f1ceb1f9ee8421122ac2
nathankang413 <nathankang413@gmail.com>
2023-12-04 14:28:12 -0800

    Generalized page change functionality

--- src/app/ScreenController.java
+++ src/app/ScreenController.java
@@ -1,9 +1,13 @@
 package app;
 
+import app.pages.controllers.student.StudentHomePageController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class ScreenController {
@@ -30,4 +34,41 @@ public class ScreenController {
 //    public static Pane getPane(String name){
 //        sc
 //    }
+
+    public static void changePage(String name, String fxml) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ScreenController.class.getResource("/fxml/student/NewStudentPage.fxml"));
+        Parent root;
+        try {
+            root = loader.load();
+        }
+        catch(Exception exception){
+            System.out.println("Failed to load fxml: " + fxml);
+            exception.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(root);
+
+        ScreenController.addScreen(name, scene);
+        ScreenController.activate(name);
+    }
+
+    public static void changeStudentPage(String name, String fxml, int student_id) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ScreenController.class.getResource(fxml));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (IOException exception) {
+            System.out.println("Failed to load fxml: " + fxml);
+            exception.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(root);
+        StudentHomePageController controller = loader.getController();
+        controller.setSID(student_id);
+
+        ScreenController.addScreen(name, scene);
+        ScreenController.activate(name);
+    }
 }
---
+++ src/app/pages/controllers/student/StudentController.java
@@ -0,0 +1,5 @@
+package app.pages.controllers.student;
+
+public interface StudentController {
+    public void setSID(int id);
+}
