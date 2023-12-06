71b1f6ccacef0bbdb59bd51da70b68e26c801097
nathankang413 <nathankang413@gmail.com>
2023-12-05 23:36:41 -0800

    Implemented new class functionality

--- src/controllers/instructor/NewClassPageController.java
+++ src/controllers/instructor/NewClassPageController.java
@@ -2,10 +2,15 @@ package controllers.instructor;
 
 import app.ScreenController;
 import controllers.UserPageController;
+import database.SqlConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class NewClassPageController implements UserPageController {
 
@@ -14,12 +19,22 @@ public class NewClassPageController implements UserPageController {
 
     @FXML
     private Button backButton, createClassButton;
+    @FXML
+    private Label errorLabel;
 
     private int instructor_id;
 
     @FXML
     protected void createClass(ActionEvent e) {
-        // TODO
+        String insertSQL = "INSERT INTO Classes (name, instructor_id, price) VALUES ('%s', '%d', '%.2f')";
+
+        try {
+            SqlConnector.runUpdate(String.format(insertSQL, classNameField.getText(), instructor_id, Double.parseDouble(priceField.getText())));
+        } catch (NumberFormatException ex) {
+            errorLabel.setText("Invalid price");
+        }
+
+        ScreenController.changeUserPage("New Event Page", "/fxml/instructor/NewEventPage.fxml", instructor_id);
     }
 
     @FXML
